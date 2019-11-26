df094038ec97af8a02b4e04223bcf16cf2c12d59
hoadq <hoadinhquoc@gmail.com>
2019-11-26 15:19:05 +0700

    *Test server

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  let responseData = {};
-  //responseData.push(request.headers);
-  response.send(request.header);
+  let responseData = [];
+  responseData.push(request.headers);
+  responseData.push(request.rawHeaders);
+  responseData.push(request.ip);
+  responseData.push(request.get('User-Agent'));
+  response.send(responseData);
+})
+
+app.get('/hello', function(request, response) {
+  var id = request.query.id;
+  response.send("Hello " + id );
 })
 
 app.listen(app.get('port'), function() {
