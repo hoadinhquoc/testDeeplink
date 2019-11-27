097265068fa49feb519873aa5e416bf8d8280df3
hoadq <hoadinhquoc@gmail.com>
2019-11-27 14:35:09 +0700

    *Test db

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var uaParser = require('ua-parser-js');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://herokuDeeplink:test12345@testdeeplink-nu9zm.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
 
 
 app.get('/', function(request, response) {
@@ -14,20 +18,17 @@ app.get('/', function(request, response) {
 
   responseData.push(request.headers['x-forwarded-for']);
 
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://herokuDeeplink:test12345@testdeeplink-nu9zm.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  response.send(collection.collectionName);
-  // perform actions on the collection object
-  client.close();
-});
-
-
-
-  //response.send(responseData);
+  client.connect(err => {
+    console.log(JSON.stringify(err));
+  
+    const db = client.db("sample_training");
+    const collection = db.collection("stories");
+  
+    //let dataPiece = collection.findOne( {id: "19970068"}, (err, result)=>{
+    let dataPiece = collection.findOne();
+  
+    console.log(JSON.stringify(dataPiece));
+  });
 })
 
 app.get('/hello', function(request, response) {
